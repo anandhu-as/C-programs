f94dfba4c7b1e761c04cbec11b15a26119f14fdc
Anandhu as <109361830+anandhu-as@users.noreply.github.com>
2023-06-08 21:13:18 +0530

    Add files via upload
    Loop Through an Array

---
+++ Array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string Array[5] = {"A", "B", "C", "D", "E"};
+    for (int i = 0; i < 5; i++)
+
+        cout << Array[i];
+
+    return 0;
+}
